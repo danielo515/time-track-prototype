3ec30e58c96fbb34230069b2c69475ae39e49217
Danielo Rodriguez <rdanielo@gmail.com>
2020-01-28 15:38:01 +0100

    Updates

--- precache-manifest.ff2a600710a2c8cf6651b155eb49c969.js
+++ precache-manifest.ff2a600710a2c8cf6651b155eb49c969.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0b0871cd75fb87842b9243a36da98788",
+    "revision": "27a09a7ed78befc7d0cac3436b976489",
     "url": "https://danielorodriguez.com/time-track-prototype/index.html"
   },
   {
-    "revision": "784b2646771defd6e9b4",
+    "revision": "4e1a8c2fa955a8762aae",
     "url": "https://danielorodriguez.com/time-track-prototype/static/css/main.f2b848ad.chunk.css"
   },
   {
-    "revision": "e7980429baf865d997ea",
-    "url": "https://danielorodriguez.com/time-track-prototype/static/js/2.7964a9e6.chunk.js"
+    "revision": "413c0d006e93d9a50402",
+    "url": "https://danielorodriguez.com/time-track-prototype/static/js/2.d3fe4a10.chunk.js"
   },
   {
     "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
-    "url": "https://danielorodriguez.com/time-track-prototype/static/js/2.7964a9e6.chunk.js.LICENSE"
+    "url": "https://danielorodriguez.com/time-track-prototype/static/js/2.d3fe4a10.chunk.js.LICENSE"
   },
   {
-    "revision": "784b2646771defd6e9b4",
-    "url": "https://danielorodriguez.com/time-track-prototype/static/js/main.2b7009d3.chunk.js"
+    "revision": "4e1a8c2fa955a8762aae",
+    "url": "https://danielorodriguez.com/time-track-prototype/static/js/main.79cca1af.chunk.js"
   },
   {
     "revision": "443fe485a6026ac934e7",
