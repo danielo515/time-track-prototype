ee7168017f8cf7df05b9518c669c1c52c6229085
Danielo Rodriguez <rdanielo@gmail.com>
2020-01-28 15:18:42 +0100

    Updates

---
+++ precache-manifest.6dd272e81af505271e0493cfde42c9f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46887ccd8d16dd0c8c68528ae85c588d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad84d04859d545171138",
+    "url": "/static/css/main.f2b848ad.chunk.css"
+  },
+  {
+    "revision": "e7980429baf865d997ea",
+    "url": "/static/js/2.7964a9e6.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/static/js/2.7964a9e6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ad84d04859d545171138",
+    "url": "/static/js/main.91f8fa81.chunk.js"
+  },
+  {
+    "revision": "4262e2f2a68593417349",
+    "url": "/static/js/runtime-main.1b5303de.js"
+  }
+]);
