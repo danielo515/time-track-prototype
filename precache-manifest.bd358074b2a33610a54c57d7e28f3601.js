f84216cf534acf6fb3d003729b12a1cdc9bf3743
Danielo Rodriguez <rdanielo@gmail.com>
2020-01-28 15:25:52 +0100

    Updates

---
+++ precache-manifest.bd358074b2a33610a54c57d7e28f3601.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d21978ffb4c34d0f7d4b50452b52d981",
+    "url": "https://danielorodriguez.com/time-track-prototype/index.html"
+  },
+  {
+    "revision": "ad84d04859d545171138",
+    "url": "https://danielorodriguez.com/time-track-prototype/static/css/main.f2b848ad.chunk.css"
+  },
+  {
+    "revision": "e7980429baf865d997ea",
+    "url": "https://danielorodriguez.com/time-track-prototype/static/js/2.7964a9e6.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "https://danielorodriguez.com/time-track-prototype/static/js/2.7964a9e6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ad84d04859d545171138",
+    "url": "https://danielorodriguez.com/time-track-prototype/static/js/main.91f8fa81.chunk.js"
+  },
+  {
+    "revision": "443fe485a6026ac934e7",
+    "url": "https://danielorodriguez.com/time-track-prototype/static/js/runtime-main.fad6d875.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6dd272e81af505271e0493cfde42c9f8.js"
+  "https://danielorodriguez.com/time-track-prototype/precache-manifest.bd358074b2a33610a54c57d7e28f3601.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://danielorodriguez.com/time-track-prototype/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
